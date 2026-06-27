35665a66bc6b7f271b328aa318776d30859c10c4
madbark67 <239074869+madbark67@users.noreply.github.com>
2026-06-26 21:58:52 -0400

    Add append and prepend methods to linkedlist

---
+++ LinkedList.rb
@@ -0,0 +1,29 @@
+class LinkedList
+  def initialize
+    @head = nil
+    @tail = nil
+  end
+
+  def append(value)
+    new_node = Node.new(value)
+    if @head.nil?
+      @head = new_node
+      @tail = new_node
+    else
+      @tail.next_node = new_node
+      @tail = new_node
+    end
+  end
+
+  def prepend(value)
+    new_node = Node.new(value)
+    if @head.nil?
+      @head = new_node
+      @tail = new_node
+    else
+      temp_node = @head
+      @head = new_node
+      @head.next_node = temp_node
+    end
+  end
+end
