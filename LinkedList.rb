3d49da749ad601779dffc8e38e57ef50116056f2
madbark67 <239074869+madbark67@users.noreply.github.com>
2026-06-29 21:06:07 -0400

    Add head,tail,at,pop and contains methods

--- LinkedList.rb
+++ LinkedList.rb
@@ -26,4 +26,63 @@ class LinkedList
       @head.next_node = temp_node
     end
   end
+
+  def size
+    count = 0
+    current = @head
+    while current
+      count = count + 1
+      current = current.next_node
+    end
+    count
+  end
+
+  def head 
+    if @head == nil
+      return nil
+    end
+    @head.value
+  end
+
+  def tail 
+    if @tail == nil
+      return nil
+    end
+    @tail.value
+  end
+
+  def at(index)
+    current = @head
+    while index > 0 && current
+      current = current.next_node
+      index = index - 1
+    end
+    if current == nil
+      return nil
+    end
+    current.value
+  end
+
+  def pop
+    if @head == nil
+      return nil
+    end
+    node = @head
+    @head = @head.next_node
+    if @head == nil
+      @tail = nil
+    end
+    node.value
+  end
+
+  def contains?(value)
+    current = @head
+    while current
+      if current.value == value
+        return true
+      end
+      current = current.next_node
+    end
+    false
+  end
 end
