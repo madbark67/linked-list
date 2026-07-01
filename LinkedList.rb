20532f848d6acf600178a3ee3eea091b24ffa032
madbark67 <239074869+madbark67@users.noreply.github.com>
2026-06-30 22:14:41 -0400

    Add insert_at method

--- LinkedList.rb
+++ LinkedList.rb
@@ -1,4 +1,6 @@
 class LinkedList
+  attr_accessor :head, :tail
+
   def initialize
     @head = nil
     @tail = nil
@@ -37,14 +39,14 @@ class LinkedList
     count
   end
 
-  def head 
+  def head_val 
     if @head == nil
       return nil
     end
     @head.value
   end
 
-  def tail 
+  def tail_val 
     if @tail == nil
       return nil
     end
@@ -84,5 +86,75 @@ class LinkedList
       current = current.next_node
     end
     false
+  end
+
+  def index(value)
+    current = @head
+    index = 0
+    while current
+      if current.value == value
+        return index
+      end
+      index = index + 1
+      current = current.next_node
+    end
+    nil
+  end
+
+  def to_s
+    string = ''
+    current = @head
+    while current
+      string.concat("( #{current.value} ) -> ")
+      current = current.next_node
+      if current == nil
+        string.concat('nil')
+      end
+    end
+    string
+  end
+
+  def insert_at(index, *values)
+    return if values.empty?
+    list_size = self.size
+
+    if index < 0
+      raise IndexError, "Index #{index} is out of bounds!"
+    end
+    if index > list_size
+      raise IndexError, "Index #{index} is out of bounds!"
+    end
+
+    new_list = LinkedList.new()
+    values.each do |value|
+      new_list.append(value)
+    end
+
+    if index == 0
+      if @tail.nil?
+        @head = new_list.head
+        @tail = new_list.tail
+      else
+        new_list.tail.next_node = @head
+        @head = new_list.head
+      end
+      return
+    elsif index == list_size
+      @tail.next_node = new_list.head
+      @tail = new_list.tail 
+      return
+    end
+
+    count = 0
+    current = @head
+    previous = nil
+    while count != index && current.next_node
+      previous = current
+      current = current.next_node
+      count = count + 1
+    end
+    
+    previous.next_node = new_list.head
+    new_list.tail.next_node = current
   end
 end
